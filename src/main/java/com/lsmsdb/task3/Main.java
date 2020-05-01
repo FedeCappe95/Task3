d561710622dc59a8053d34423a3a6ab083c8df78
FedeCappe95 <57992304+FedeCappe95@users.noreply.github.com>
2020-05-01 15:43:47 +0200

    Initial commit

---
+++ src/main/java/com/lsmsdb/task3/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lsmsdb.task3;
+
+/**
+ *
+ * @author Federico
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
