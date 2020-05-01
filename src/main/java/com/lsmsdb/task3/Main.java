4d0e086070a2183efae736d3137494f97a8e3948
FedeCappe95 <57992304+FedeCappe95@users.noreply.github.com>
2020-05-01 17:14:17 +0200

    Update Main.java

--- src/main/java/com/lsmsdb/task3/Main.java
+++ src/main/java/com/lsmsdb/task3/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.lsmsdb.task3;
 
 import com.lsmsdb.task3.ui.SplashScreen;
@@ -21,8 +16,7 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 /**
- *
- * @author Federico
+ * Main class (entry point) of the application
  */
 public class Main extends Application {
     
