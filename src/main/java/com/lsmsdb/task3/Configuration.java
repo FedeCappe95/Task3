a119880ea264dc4f5f741d38d2bdeb2950bff660
FedeCappe95 <57992304+FedeCappe95@users.noreply.github.com>
2020-05-03 18:00:53 +0200

    Aggiunta configurazione in JSON

--- src/main/java/com/lsmsdb/task3/Configuration.java
+++ src/main/java/com/lsmsdb/task3/Configuration.java
@@ -1,7 +1,11 @@
 package com.lsmsdb.task3;
 
+import com.lsmsdb.task3.utils.Utils;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONObject;
 
 /**
  * Class that contains all the configurable parameter
@@ -10,7 +14,9 @@ public class Configuration {
     
     private static long VALIDITY_PERIOD = 2*7*24*60*60*1000; //2 weeks
     private static long USER_MOST_CRITICAL_PLACES_NUMBER = 5;
+    
     private static final Map<String,Integer> DISTANCE_LOOKUP_TABLE;
+    private static final String configurationFilePath = "/otherResources/configuration.json";
     
     static {
         DISTANCE_LOOKUP_TABLE = new HashMap<>();
@@ -19,6 +25,22 @@ public class Configuration {
         DISTANCE_LOOKUP_TABLE.put("Medium", 6);
         DISTANCE_LOOKUP_TABLE.put("Distant", 8);
         DISTANCE_LOOKUP_TABLE.put("Very distant", 10);
+    }
+    
+    public static void loadFromClasspath() {
+        try {
+            JSONObject jsonObject = new JSONObject(
+                    Utils.readAsStringFromClasspath(configurationFilePath)
+            );
+            VALIDITY_PERIOD = Long.parseLong(jsonObject.getString("VALIDITY_PERIOD"));
+            USER_MOST_CRITICAL_PLACES_NUMBER = Long.parseLong(jsonObject.getString("USER_MOST_CRITICAL_PLACES_NUMBER"));
+        }
+        catch(Exception ex) {
+            Logger.getLogger(Configuration.class.getName()).log(
+                    Level.WARNING,
+                    "Error loading configuration, default configuration will be used"
+            );
+        }
     }
     
     /**
